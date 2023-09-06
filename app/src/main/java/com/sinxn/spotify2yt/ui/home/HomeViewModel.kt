db5756ce26075ad55c2413ebd46a42e89749fb96
sinan <sinan9539391118@gmail.com>
2023-09-06 15:20:47 +0530

    Added view ytmusic app on click from playlist screen

--- app/src/main/java/com/sinxn/spotify2yt/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sinxn/spotify2yt/ui/home/HomeViewModel.kt
@@ -3,45 +3,34 @@ package com.sinxn.spotify2yt.ui.home
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.adamratzman.spotify.SpotifyApi
 import com.adamratzman.spotify.SpotifyAppApi
 import com.adamratzman.spotify.SpotifyAppApiBuilder
 import com.adamratzman.spotify.SpotifyCredentials
-import com.adamratzman.spotify.models.PlaylistTrack
+import com.adamratzman.spotify.models.Track
+import com.google.gson.JsonObject
 import com.sinxn.spotify2yt.repository.SharedPref
+import com.sinxn.spotify2yt.tools.similarityTo
+import com.sinxn.spotify2yt.tools.ytId
+import com.sinxn.spotify2yt.ytmibrary.YTMusic
+import com.sinxn.spotify2yt.ytmibrary.mixins.SearchMixin
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.io.File
+import java.util.Locale
 import javax.inject.Inject
+import kotlin.math.abs
 
-data class Playlist(
-    val id: String,
-    val collaborative: Boolean,
-    val followers: Followers,
-    val images: List<Image>,
-    val tracks: Tracks
-)
-
-data class Followers(
-    val total: Int
-)
-
-data class Image(
-    val height: Int,
-    val width: Int
-)
-
-
-data class Tracks(
-    val total: Int
-)
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
    private val sharedPref: SharedPref,
+    private val storage: File
 ): ViewModel()
 {
     private var spotifyAppApi: SpotifyAppApi? = null
-    var playlistSongs: List<PlaylistTrack> = emptyList()
+    private lateinit var ytmApi: YTMusic
+
+    var playlistSongs: List<Track?> = emptyList()
     var playlistName: String = ""
 
     fun onConvert(playlist: String) {
@@ -49,14 +38,46 @@ class HomeViewModel @Inject constructor(
             val playlistId = playlist.trim('/').split('/').last()
             val res = spotifyAppApi?.playlists?.getPlaylist(playlistId)
             res?.let {
-                playlistSongs = res.tracks.items
                 playlistName = res.name
+                playlistSongs = res.tracks.items.map {
+                   it.track?.asTrack
+                }
             }
-
-
+            getYTSongs()
         }
+    }
 
 
+    private fun getYTSongs() {
+        viewModelScope.launch {
+            playlistSongs.forEach { track ->
+                require(track!=null) {"track is null"}
+                val songName = track.name.replace(Regex(" \\(feat.*\\..+\\)"), "")
+                var query = track.artists.joinToString{it.name + " " } + songName
+                query = query.replace("&", "")
+                val res = SearchMixin(ytmApi).search(query)
+                if (res.isNotEmpty()) {
+                    Log.d("TAG", "getYTSongs: $res")
+
+                    val targetSongId = getTopResult(res)
+                    if (!targetSongId.isNullOrEmpty()) track.ytId = targetSongId
+                    Log.d("TAG", "getYTSongs: $targetSongId")
+                }
+
+
+            }
+        }
+    }
+
+
+    private fun getTopResult(res: List<JsonObject>): String? {
+        res.forEach {
+            if (it["category"].asString.equals("Top result"))
+                return if (it.has("videoId"))
+                    it["videoId"].asString
+                else null
+        }
+        return null
     }
 
     var isLogged = false
@@ -64,24 +85,83 @@ class HomeViewModel @Inject constructor(
     init {
         isLogged = sharedPref.isLogged()
         if (isLogged) {
-            if (sharedPref.expiresAt< System.currentTimeMillis()/1000) {
-                //refreshToken()
+            viewModelScope.launch {
+                ytmApi = YTMusic(File(storage, "auth").path)
+
+                val spotifyCred = SpotifyCredentials().apply {
+                    clientId = sharedPref.spotifyClientId
+                    clientSecret = sharedPref.spotifyClientSecret
+                }
+                spotifyAppApi = SpotifyAppApiBuilder(spotifyCred).build()
             }
-            initYTM()
-            initSpotify()
 
         }
     }
-    private fun initSpotify() {
-        viewModelScope.launch {
-            val spotifyCred = SpotifyCredentials()
-            spotifyCred.clientId = sharedPref.spotifyClientId
-            spotifyCred.clientSecret = sharedPref.spotifyClientSecret
-            spotifyAppApi = SpotifyAppApiBuilder(spotifyCred).build()
+
+    private fun getBestFitSongId(ytmResults: List<JsonObject>, spoti: Track?): String? {
+        require(spoti!=null) {"spotify Track is null"}
+
+        val matchScore = hashMapOf<String, Int>()
+        val titleScore = hashMapOf<String, Int>()
+
+        for (ytm in ytmResults) {
+            val resultType = ytm.get("resultType")?.asString
+            val title = ytm.get("title")?.asString
+            val duration = ytm.get("duration")?.asString
+            val videoId = ytm.get("videoId")?.asString
+            val artists = ytm.getAsJsonArray("artists")?.joinToString(" ") { it.asJsonObject.get("name").asString }
+            val album = ytm.getAsJsonObject("album")?.get("name")?.asString
+
+            if (resultType !in listOf("song", "video") || title.isNullOrEmpty()) {
+                continue
+            }
+            require(videoId!=null) {"$ytm video Id null"}
+
+
+            val durationMatchScore: Int? = if (duration != null) {
+                val durationItems = duration.split(":")
+                val durationValue = durationItems[0].toInt() * 60 + durationItems[1].toInt()
+                1 - abs(durationValue - spoti.durationMs) * 2 / spoti.durationMs
+            } else {
+                null
+            }
+
+            val sanitizedTitle = if (resultType == "video") {
+                val titleSplit = title.split("-")
+                if (titleSplit.size == 2) {
+                    titleSplit[1].trim()
+                } else {
+                    title
+                }
+            } else {
+                title
+            }
+            titleScore[videoId] = sanitizedTitle.lowercase(Locale.ROOT).similarityTo(spoti.name.lowercase(Locale.ROOT))
+            val scores = mutableListOf(
+                titleScore[videoId]?:0,
+                spoti.artists.joinToString{it.name + " " }.lowercase(Locale.ROOT)
+                    .let { artists?.lowercase(Locale.ROOT)?.similarityTo(it) }?: 0
+            )
+
+            durationMatchScore?.let {
+                scores.add(it * 5)
+            }
+
+            if (resultType == "song" && album != null) {
+                scores.add(album.lowercase(Locale.ROOT).similarityTo(spoti.album.name.lowercase(
+                    Locale.ROOT
+                )))
+            }
+
+            matchScore[videoId] =
+                (scores.average() * maxOf(1, if (resultType == "song") 2 else 1)).toInt()
         }
 
-    }
-    private fun initYTM() {
+        if (matchScore.isEmpty()) {
+            return null
+        }
 
+        return matchScore.maxByOrNull { it.value }?.key
     }
+
 }
--- app/src/main/java/com/sinxn/spotify2yt/ui/home/PlayListScreen.kt
+++ app/src/main/java/com/sinxn/spotify2yt/ui/home/PlayListScreen.kt
@@ -1,6 +1,9 @@
 package com.sinxn.spotify2yt.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -9,20 +12,27 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.adamratzman.spotify.models.PlaylistTrack
+import com.adamratzman.spotify.models.Track
+import com.sinxn.spotify2yt.R
+import com.sinxn.spotify2yt.tools.ytId
 
 @Composable
 fun PlayListScreen(
     navController: NavController,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
+    val context = LocalContext.current
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Column(
             Modifier
@@ -31,20 +41,30 @@ fun PlayListScreen(
             Text(text = viewModel.playlistName)
         }
         LazyColumn(content = {
-            Log.d("TAG", "PlayListScreen: ${viewModel.playlistSongs}")
             items(viewModel.playlistSongs) {
-                AlbumCard(playlistTrack = it)
+                AlbumCard(playlistTrack = it) {track->
+                    val url = "https://music.youtube.com/watch?v=${track?.ytId}"
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    context.startActivity(intent)
+                }
             }
         })
     }
 }
 
 @Composable
-fun AlbumCard(playlistTrack: PlaylistTrack) {
+fun AlbumCard(
+    playlistTrack: Track?,
+    onClick: (Track?) -> Unit
+) {
     Column(
         Modifier
             .fillMaxWidth(0.8f)
-            .padding(15.dp),) {
-        Text(text =playlistTrack.track?.asTrack?.name?: "" )
+            .padding(15.dp)
+            .clickable {
+                onClick(playlistTrack)
+            },) {
+        Text(text = playlistTrack?.name?: "" )
+
     }
 }
