4eb224114896fa368a5314c2f3c3f1f454d2d85e
sinan <sinan9539391118@gmail.com>
2023-11-07 15:37:22 +0530

    added thumbnail in PlayListScreen play button to fab button

--- app/src/main/java/com/sinxn/youtify/ui/home/PlayListScreen.kt
+++ app/src/main/java/com/sinxn/youtify/ui/home/PlayListScreen.kt
@@ -4,13 +4,12 @@ import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -21,6 +20,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconToggleButton
@@ -34,6 +34,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -42,9 +43,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.sinxn.youtify.R
 import com.sinxn.youtify.domain.model.Tracks
 import com.sinxn.youtify.ui.home.components.UploadPlaylistDialog
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun PlayListScreen(
@@ -63,39 +68,58 @@ fun PlayListScreen(
 
     val uiState = viewModel.uiState
     val context = LocalContext.current
-    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.25f)) {
-            Text(text = uiState.playlist?.name?: stringResource(R.string.playlist_name_not_found),fontWeight = FontWeight.Bold, fontSize = 24.sp ,modifier = Modifier.align(Alignment.Center))
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(15.dp)
-                .align(Alignment.BottomEnd), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.End) {
-                Text(text = "${uiState.playlistSongs.size}/${uiState.playlist?.count?:0}")
-                IconButton(onClick = {  }) {
-                    Icon(painter = painterResource(id = R.drawable.playlist_save_ic), contentDescription = stringResource(R.string.save_playlist))
-                }
-                if (uiState.playlist?.youtube_id==null) {
-                    IconButton(onClick = { uploadDialogState = true }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.playlist_upload_ic),
-                            contentDescription = stringResource(R.string.upload_playlist)
-                        )
-                    }
-                } else {
-                    IconButton(onClick = { viewModel.onEvent(PlaylistEvent.Play(uiState.playlist))}) {
-                        Icon(
-                            Icons.Filled.PlayArrow,
-                            contentDescription = stringResource(R.string.open_playlist_yt)
-                        )
-                    }
-                }
-            }
-
-        }
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally ) {
         LazyColumn(content = {
+            item { Row(
+                Modifier
+                    .fillMaxWidth().padding(vertical = 15.dp)) {
+                GlideImage(
+                    modifier = Modifier.padding(horizontal = 25.dp)
+                        .size(100.dp,100.dp),
+                    imageModel = { uiState.playlist?.image_url },
+                    imageOptions = ImageOptions(
+                        contentScale = ContentScale.Crop,
+                        alignment = Alignment.Center
+
+                    ),
+                    requestOptions = {
+                        RequestOptions()
+                            .placeholder(R.drawable.ic_app_name_colored)
+                            .override(256, 256)
+                            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                            .centerCrop()
+
+                    }
+                )
+                Column {
+                    Text(text = uiState.playlist?.name?: stringResource(R.string.playlist_name_not_found),fontWeight = FontWeight.Bold, fontSize = 24.sp,modifier = Modifier.fillMaxWidth())
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.End) {
+                        Text(text = "${uiState.playlistSongs.size}/${uiState.playlist?.count?:0}")
+                        IconButton(onClick = {  }) {
+                            Icon(painter = painterResource(id = R.drawable.playlist_save_ic), contentDescription = stringResource(R.string.save_playlist))
+                        }
+                        if (uiState.playlist?.youtube_id==null) {
+                            IconButton(onClick = { uploadDialogState = true }) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.playlist_upload_ic),
+                                    contentDescription = stringResource(R.string.upload_playlist)
+                                )
+                            }
+                        } else {
+                            FloatingActionButton(onClick = { viewModel.onEvent(PlaylistEvent.Play(uiState.playlist))}) {
+                                Icon(
+                                    Icons.Filled.PlayArrow,
+                                    contentDescription = stringResource(R.string.open_playlist_yt)
+                                )
+                            }
+                        }
+                    }
+                }
+
+
+            } }
             items(uiState.playlistSongs) {
                 AlbumCard(playlistTrack = it,  onAction = {event->
                     viewModel.onEvent(event)
@@ -131,8 +155,8 @@ fun AlbumCard(
     Card(
         colors = if (ytIdNotFound) CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.errorContainer, ) else CardDefaults.cardColors(),
         modifier = Modifier
-            .fillMaxWidth(0.9f)
-            .padding(0.dp, 5.dp)
+            .fillMaxWidth()
+            .padding(10.dp, 5.dp)
             .clickable {
                 onAction(SongEvent.Play(playlistTrack))
             }) {
